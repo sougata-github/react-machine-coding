24386a435738637924174315667ceb90116a8ee5
Sougata <sougatadas9874@gmail.com>
2024-12-23 11:22:46 +0530

    modify FileUploader.

--- src/components/file-uploads/FileUploader.jsx
+++ src/components/file-uploads/FileUploader.jsx
@@ -71,7 +71,7 @@ const FileUploader = () => {
         </div>
       )}
 
-      {file && (
+      {file && status !== "success" && (
         <button
           onClick={handleFileUpload}
           className="w-[120px] px-4 py-2 rounded-lg bg-black text-white text-sm"
